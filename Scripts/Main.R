2e5dd6fb67d6b570e736405cafd53dae2d520ead
David Torgerson <David@Davids-MacBook-Pro.local>
2021-02-04 16:15:53 -0500

    Building multiple models

--- Scripts/Main.R
+++ Scripts/Main.R
@@ -147,8 +147,8 @@ z_scale = function(x) {
 }
 
 recent_daily_incidents_wide = recent_daily_incidents %>%
-  spread(incident, crime_count) %>% #This de-duplicates the incident dates
-  mutate_at(vars(ASSAULT:UUV), list(z_scale)) #Turning crime counts into z scores to be able to compare them to each other
+  spread(incident, crime_count) #This de-duplicates the incident dates
+  #mutate_at(vars(ASSAULT:UUV), list(z_scale)) #Turning crime counts into z scores to be able to compare them to each other
 
 rocv_by_crime2 = recent_daily_incidents_wide %>%
   rolling_origin(
@@ -180,7 +180,9 @@ train_test_splits[1] #Pulling out the first set of training and testing data
 
 ################## END Creating Train/Test Data #################
 
+####################################################
 ################# Building Model ###################
+####################################################
 
 library(h2o)
 
@@ -188,6 +190,7 @@ h2o.init()
 
 sample_rocv = train_test_splits[[1]]
 
+################# General Linear Model #####################
 map(sample_rocv, function(x){
   
   train = sample_rocv$train %>%
@@ -197,7 +200,7 @@ map(sample_rocv, function(x){
     mutate(incident = factor(incident)) #Converting crimes to categories
   
   features = train %>%
-    select(-crime_count) %>%
+    select(-crime_count, -incident_date) %>%
     colnames()
   
   target = "crime_count"
@@ -221,3 +224,94 @@ map(sample_rocv, function(x){
   #Variable Importance
   h2o.varimp(model)
 })
+
+###################### Gradient Boosted Machine (GBM) ###################
+map(sample_rocv, function(x){
+  
+  train = sample_rocv$train %>%
+    mutate_at(vars(month:incident), list(as.factor)) #Converting crimes to categories
+  
+  test = sample_rocv$test %>%
+    mutate_at(vars(month:incident), list(as.factor)) #Converting crimes to categories
+  
+  features = train %>%
+    select(-crime_count, -incident_date) %>%
+    colnames()
+  
+  target = "crime_count"
+  
+  model = h2o.gbm(
+    x = features,
+    y = target,
+    training_frame = as.h2o(train),
+    validation_frame = as.h2o(test),
+    ntrees = 50, #How many trees do you want the model to build, Default set at 50
+    learn_rate = 0.1, #Default set at 0.1
+    max_depth = 5, #Default set at 5
+    categorical_encoding = "AUTO", #Default set at AUTO
+    distribution = "poisson" #Used for non-negative count data
+  )
+  
+  #Performance Metrics
+  performance_metrics = h2o.performance(model, valid = TRUE)
+  
+  performance_metrics_tbl = tibble(
+    RMSE = performance_metrics@metrics$RMSE,
+    R2 = performance_metrics@metrics$r2
+  ) #})
+  
+  #Variable Importance
+  h2o.varimp(model)
+  
+  predictions = h2o.predict(model, newdata = as.h2o(test)) %>%
+    as.vector()
+  
+  test %>%
+    mutate(Predictions = predictions)
+})
+
+################## Random Forest Model ################
+map(sample_rocv, function(x){
+  
+  train = sample_rocv$train %>%
+    mutate_at(vars(month:incident), list(as.factor)) #Converting crimes to categories
+  
+  test = sample_rocv$test %>%
+    mutate_at(vars(month:incident), list(as.factor)) #Converting crimes to categories
+  
+  features = train %>%
+    select(-crime_count, -incident_date) %>%
+    colnames()
+  
+  target = "crime_count"
+  
+  model = h2o.randomForest(
+    x = features,
+    y = target,
+    training_frame = as.h2o(train),
+    validation_frame = as.h2o(test),
+    ntrees = 50, #How many trees do you want the model to build, Default set at 50
+    max_depth = 20, #Default set at 20
+    categorical_encoding = "AUTO", #Default set at AUTO
+  ) 
+
+  #Performance Metrics
+  performance_metrics = h2o.performance(model, valid = TRUE)
+  
+  performance_metrics_tbl = tibble(
+    RMSE = performance_metrics@metrics$RMSE,
+    R2 = performance_metrics@metrics$r2
+  ) #})
+  
+  #Variable Importance
+  h2o.varimp(model)
+  
+  predictions = h2o.predict(model, newdata = as.h2o(test)) %>%
+    as.vector()
+  
+  test %>%
+    mutate(Predictions = predictions)
+})
+
+##### GBM has a lower RMSE so we will move forward with that model
+
