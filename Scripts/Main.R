cf69cb2895f557f7ece2b3ff410961947073f50c
David Torgerson <David@Davids-MacBook-Pro.local>
2021-02-05 12:19:27 -0500

    Running GBM model and calculating MASE metrics.

--- Scripts/Main.R
+++ Scripts/Main.R
@@ -314,4 +314,63 @@ map(sample_rocv, function(x){
 })
 
 ##### GBM has a lower RMSE so we will move forward with that model
- 
+
+############# Model Implementation ##############
+map(train_test_splits, function(x){
+  
+  train = x$train %>%
+    mutate_at(vars(month:incident), list(as.factor)) #Converting crimes to categories
+  
+  test = x$test %>%
+    mutate_at(vars(month:incident), list(as.factor)) #Converting crimes to categories
+  
+  features = train %>%
+    select(-crime_count, -incident_date) %>%
+    colnames()
+  
+  target = "crime_count"
+  
+  model = h2o.gbm(
+    x = features,
+    y = target,
+    training_frame = as.h2o(train),
+    validation_frame = as.h2o(test),
+    ntrees = 50, #How many trees do you want the model to build, Default set at 50
+    learn_rate = 0.1, #Default set at 0.1
+    max_depth = 5, #Default set at 5
+    categorical_encoding = "AUTO", #Default set at AUTO
+    distribution = "poisson" #Used for non-negative count data
+  )
+  
+  #Simple Predictions Calculation
+  simple_model = train %>%
+    arrange(incident, incident_date) %>% #Sorting by incident and date
+    group_by(incident) %>%
+    slice(tail(row_number(), 14)) %>% #Grab the last 14 rows of each incident
+    ungroup() %>%
+    mutate(incident_date = incident_date + days(14)) %>% #Adding 14 days to merge with testing data
+    select(incident_date, incident, Simple = crime_count)
+  
+  #Predictions from GBM Model
+  predictions = h2o.predict(model, newdata = as.h2o(test)) %>%
+    as.vector()
+  
+  #MASE Calculation
+  MASE_by_incident = test %>%
+    mutate(Predictions = predictions) %>% #Adding predictions from gbm 
+    inner_join(simple_model, by = c("incident","incident_date")) %>% #adding simple predictions
+    group_by(incident) %>%
+    summarise(
+      GBM_MAE = mean(abs(Predictions - crime_count)), #Getting Mean Average Error for GBM
+      Simple_MAE = mean(abs(Simple - crime_count)) #Getting Mean Average Error for Simple
+    ) %>%
+    mutate(MASE = GBM_MAE/Simple_MAE) #Calculating MASE
+  
+  #Reporting out Metrics
+  out = MASE_by_incident
+  
+  return(out)
+})
+
+
+
