11371d351bd5242fd12c0801fda1e97ca71dc320
David Torgerson <David@Davids-MacBook-Pro.local>
2021-03-01 13:34:54 -0500

    added more code

--- Scripts/gbm_tuning.R
+++ Scripts/gbm_tuning.R
@@ -4,7 +4,7 @@ library(h2o)
 
 h2o.init()
 
-best_parameters = map(train_test_splits, function(x){
+rocv_tuning = map(train_test_splits, function(x){
   
   train = x$train %>%
     mutate_at(vars(month:incident), list(as.factor)) #Converting crimes to categories
@@ -36,22 +36,51 @@ best_parameters = map(train_test_splits, function(x){
                       seed = 1,
                       hyper_params = gbm_params)
   
-  
-  gbm_best_grid = h2o.getGrid(grid_id = gbm_grid,
-                              sort_by = "auc",
+  gbm_grid_sort = h2o.getGrid(grid_id = gbm_grid@grid_id, #Gets auto generated grid_id - this could also be manually listed in the h2o.grid call and passed through to this call.
+                              sort_by = "rmse",
                               decreasing = TRUE)
   
-  print(gbm_best_grid)
+  best_model_id = gbm_grid_sort@model_ids[[1]]
   
-  best_gbm = h2o.getModel(gbm_best_grid@model_ids[[1]])
+  best_gbm = h2o.getModel(best_model_id)
+  
+  best_params = gbm_grid_sort@summary_table %>%
+    as_tibble() %>%
+    mutate_at(vars(-model_ids), list(as.numeric)) %>%
+    filter(model_ids == best_model_id)
+  
+  worst_params = gbm_grid_sort@summary_table %>%
+    as_tibble() %>%
+    mutate_at(vars(-model_ids), list(as.numeric)) %>%
+    arrange(desc(rmse)) %>%
+    slice(1)
+  
+  other_params_not_tuned = best_gbm@model$model_summary %>%
+    as_tibble() %>%
+    select(-any_of(colnames(best_params)))
+  
+  best_params_with_metrics = best_params %>%
+    bind_cols(other_params_not_tuned)
   
   #Clean out the h2o cluster.
   h2o.removeAll()
   
   #Reporting out Metrics
-  out = best_gbm
+  out = list(best_params_with_metrics = best_params_with_metrics,
+             worst_params = worst_params)
   
   return(out)
 })
 
+best_params = map(rocv_tuning, function(x) x[["best_params_with_metrics"]]) %>%
+  bind_rows() %>%
+  unite(col = "Param_String", col_sample_rate:sample_rate, remove = F)
 
+most_frequent_best_params = best_params %>%
+  count(Param_String) %>%
+  filter(n == max(n))
+
+best_params %>%
+  semi_join(most_frequent_best_params, by = "Param_String") %>%
+  select(col_sample_rate:sample_rate, number_of_trees, min_depth) %>%
+  distinct()
